a340937ccc0031073bfbe416c7d5d19d3cceaa56
Teck <kongteckonn@gmail.com>
2026-03-28 10:52:51 +0800

    Document structure usage in Structure.c
    Add comments and example for structure usage in C

---
+++ Structure.c
@@ -0,0 +1,28 @@
+/* Structure */
+// 结构体
+// 是一些值的集合，每个成员可以是不同类型
+// 数组里面都是相同类型
+
+//struct tag// 标签
+//{
+//	/*member_list* / ;
+//}/*variable_value*/;
+
+// example
+
+//struct Pao // 声明结构体 的类型(不占内存空间)
+//{
+//	char name[20];
+//	char tele[20];
+//	char sex[7];
+//	int high;
+//	int weight;
+//} p1 , p2 ;// p1, p2 是全局变量(少用)，结构体变量
+//
+//int main()
+//{
+//	struct Pao p3 = { "Joo Wen", "01110522039", "Female", 168, 68};// 创建了，才占空间
+//	printf("%s %s %s %dcm %dkg", p3.name, p3.tele, p3.sex, p3.high, p3.weight);
+//	// 对象(.) 指针(->)
+//	return 0;
+//}
