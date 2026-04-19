6bf478bdc29cd1735a82c5cdb494562845fd4df4
Teck <kongteckonn@gmail.com>
2026-04-19 22:04:35 +0800

    Refactor I_and_O_stream.cpp with consistent formatting

--- I_and_O_stream.cpp
+++ I_and_O_stream.cpp
@@ -47,3 +47,62 @@ using namespace std;
 
 // same as cin.get() but it will ignore the delimiter 
 // and the delimiter will not be stored in the array
+
+/* cout */
+
+// cout.setf(ios::scientific)
+// cout.width()
+// cout.fill()
+
+// int main()
+// {
+//     cout.setf(ios::scientific); // scientific to output float value
+//     cout.width(20);
+//     cout << 1.265 << endl;
+//     cout.fill('*');
+//     return 0;
+// }
+
+// hex dec oct
+
+// int main()
+// {
+//     cout.setf(ios::showbase | ios::uppercase);
+//     cout << hex <<  520 << endl;
+//     cout << oct <<  520 << endl;
+//     cout << dec <<  520 << endl;
+//     return 0;
+// }
+
+/* #include<sstream> */
+
+// #include<sstream>
+// int main()
+// {
+//     int num = 12345;
+//     char str[25];
+//     stringstream stream;
+//     stream << num << '\0';
+//     cout << str << endl;
+//     return 0;
+// }
+
+/* File Class */
+
+// #include<fstream>
+// void Acsii()
+// {
+//     ifstream input("./in.txt");
+//     // determine whather open or not 
+//     if(!input.is_open())
+//     {
+//         cerr << "Fail to open" << endl;
+//     }
+//     else cout << "Sucessful" << endl;
+//     input.close();
+// }
+// int main()
+// {
+//     Acsii();
+//     return 0;
+// }
